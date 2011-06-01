d2f0cc45c3562ed24313619ed0468326aceecbf2
root <root@static.113.99.4.46.clients.your-server.de>
2011-06-01 11:15:10 +0200

    ...

--- lib/spider_helper.rb
+++ lib/spider_helper.rb
@@ -65,13 +65,13 @@ class SpiderHelper
     return if doc.nil?
     doc.search("a").each do  |a|
       target_link = URI.parse(a.attributes["href"].value) rescue next
+      next if a.attributes["href"].value.include? "mailto:"
       nofollow = a.attributes["rel"].value.include?("nofollow") rescue false
       title = a.attributes["title"].value rescue ""
       inner_text = a.inner_text
 
       next if @ignore.any? {|keyword| target_link.to_s.include? keyword}
       next if target_link == "" or target_link.nil?
-      next if target_link.include? "mailto:"
       target_link = URI.join page.url, target_link rescue next
 
       target_link = normalize_link(target_link)
